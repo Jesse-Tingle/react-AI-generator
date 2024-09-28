e5230caa97b84656535211ba18756d9c6898419e
Jesse-Tingle <jtingle0@gmail.com>
2024-09-28 17:45:38 -0400

    set up boiler plate react app

---
+++ ai-generator/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+// import { useState } from "react"
+import "./App.css"
+console.log(import.meta.env.VITE_MY_API_KEY)
+
+function App() {
+	return (
+		<div className="App">
+			<h1>AI Image Generator</h1>
+		</div>
+	)
+}
+
+export default App
